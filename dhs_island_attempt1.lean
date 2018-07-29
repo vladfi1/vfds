d70407334932a668192d64ef737c212705426a9c
Daniel Selsam <daniel.selsam@protonmail.com>
2018-07-29 08:25:26 +0100

    bug in lean, local constants in decl

--- dhs_island_attempt1.lean
+++ dhs_island_attempt1.lean
@@ -42,8 +42,8 @@ parameter initial_oracle  : common_knowledge 1 (marked_ones.card > 0) N
 
 parameter no_one_leaves   : ∀ (n : person) (t : timestep), t < N → ¬ knows t n (is_marked n)
 
-theorem base_case : holds (marked_ones.card = 1) → ∀ (n : person), holds (is_marked n) → knows 1 n (is_marked n) :=
-assume (H_card : holds (marked_ones.card = 1)) (n : person) (H_n : holds (is_marked n)),
+theorem base_case : ∀ (n : person), holds (is_marked n) → holds (marked_ones.card = 1) → knows 1 n (is_marked n) :=
+assume (n : person) (H_n : holds (is_marked n)) (H_card : holds (marked_ones.card = 1)),
 
 have H₁ : common_knowledge 1 (marked_ones.card > 0) N, from initial_oracle,
 have H₂ : common_knowledge 1 (marked_ones.card > 0) 0, from common_knowledge_loosen sorry H₁,
@@ -60,15 +60,23 @@ have H_10 : knows 1 n (is_marked n ∧ marked_ones.card = 1), from all_rational 
 have H_11 : (is_marked n ∧ marked_ones.card = 1) → is_marked n, from and.left,
 show knows 1 n (is_marked n), from all_rational H_10 H_11
 
-theorem islanders : ∀ (M : ℕ), holds (marked_ones.card = M+1) → ∀ (n : person), holds (is_marked n) → knows (M+1) n (is_marked n) :=
+theorem step_case : ∀ (M : ℕ) (n : person) (H_n : holds (is_marked n)),
+                      (holds (marked_ones.card = M+1) → knows (M+1) n (is_marked n))
+                    → (holds (marked_ones.card = M+2) → knows (M+2) n (is_marked n)) :=
+sorry
+
+theorem islanders : ∀ (M : ℕ) (n : person) (H_n : holds (is_marked n)), holds (marked_ones.card = M+1) → knows (M+1) n (is_marked n) :=
 begin
-intros M H_card n H_n,
+intros M,
 induction M with M IHm,
-apply base_case H_card n H_n,
-exact sorry
-
-
-
+apply base_case,
+intros n H_n H_card,
+apply step_case M n H_n,
+intro H_card2,
+apply IHm,
+exact H_n,
+exact H_card2,
+exact H_card
 end
 
 end island
